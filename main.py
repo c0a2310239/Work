63bb4600b84222446f5087a3cf2261c0259158d0
Yuunosuke Tabata <c0a2310239@edu.teu.ac.jp>
2024-11-27 16:19:16 +0900

    商品を追加する関数の作成 #1

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 def main():
-    goods = [{"name": "コーヒー", "num": 3}, {"name": "本", "num": 5}]
+    goods = [{"name": "コーヒー", "amount": 300, "num": 3}, {"name": "本", "aount": 500, "num": 5}, {"name": "クッキー", "aoount": 100, "num": 10}]
 
-    print("|商品名|商品数|")
-    print("|----|----|")
+    print("|商品名|金額|商品数|")
+    print("|----|----|----|")
     print_goods(goods)
 
 
 def print_goods(goods):
     for good in goods:
         name = good["name"]
+        amount = good["amount"]
         num = good["num"]
-        print(f"|{name}|{num}|")
+        
+        print(f"|{name}|{amount}|{num}|")
+        
+
+def add_goods(goods, name, amount, num):
+    good = {"name": name, "amount": amount, "num": num}
+    goods.append(good)
+    return goods
 
 
 if __name__ == "__main__":
